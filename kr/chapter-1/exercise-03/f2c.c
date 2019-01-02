11e99f22fc3344ffe0135cebe0ccf9ad61faeb87
Son Dang <sondangdev@gmail.com>
2019-01-02 23:23:10 +0700

    Add solution for exercise 1.3

---
+++ kr/chapter-1/exercise-03/f2c.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+ * print Fahrenheit-Celsius table
+ * for fahr = 0, 20, ..., 300
+ */
+
+int main(void)
+{
+    float fahr, celsius;
+    float lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    printf("Fahrenheit\tCelsius\n");
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%10.0f\t%7.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+    return 0;
+}
