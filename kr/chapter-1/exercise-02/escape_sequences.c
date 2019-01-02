a6a1057fff1b120d321fc68d28a37f69d4bf3a22
Son Dang <sondangdev@gmail.com>
2019-01-02 23:23:10 +0700

    Add solution for exercise 1.2

---
+++ kr/chapter-1/exercise-02/escape_sequences.c
@@ -0,0 +1,16 @@
+/*
+ * Solution for Exercise 1-2
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("newline: \n");
+    printf("tab: \t\n");
+    printf("backspace: \b\n");
+    printf("double quote: \"\n");
+    printf("backslash: \\\n");
+    /* printf("invalid escape character: \c\n"); */
+    return 0;
+}
