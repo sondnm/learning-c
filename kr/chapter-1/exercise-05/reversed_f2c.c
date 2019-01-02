8b1121d422590cd6a1bd07841f487a9b61d71b95
Son Dang <sondangdev@gmail.com>
2019-01-02 23:23:10 +0700

    Add solution for exercise 1.5

---
+++ kr/chapter-1/exercise-05/reversed_f2c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * print Fahrenheit-Celsius table
+ * for fahr = 0, 20, ..., 300
+ */
+
+int main(void)
+{
+    int fahr;
+
+    printf("Fahrenheit\tCelsius\n");
+    for ( fahr = 300; fahr >= 0; fahr = fahr - 20) {
+        printf("%10d\t%7.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    }
+    return 0;
+}
